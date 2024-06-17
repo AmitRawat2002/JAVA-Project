e1b5446482246af6d71277195460ff785b82fce3
AmitRawat2002 <rawattanay4@gmail.com>
2024-06-17 11:27:50 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hii iam amit");
+        System.out.println("hii hello");
+        System.out.println("hii");
+        System.out.println("hello");
+        System.out.println("tight");
+
+
+    }
+
+    
+    
+}
